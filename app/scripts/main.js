bbfbdebc7d75f8be9df431faeb55795206487587
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2018-01-09 21:39:12 +0000

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,81 @@
+// var movieLists = [
+// 	{
+// 		name: "New Releases",
+// 		videos: [
+// 			{
+// 				"id": 70111470,
+// 				"title": "Die Hard",
+// 				"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+// 				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+// 				"rating": 4.0,
+// 				"bookmark": []
+// 			},
+// 			{
+// 				"id": 654356453,
+// 				"title": "Bad Boys",
+// 				"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+// 				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+// 				"rating": 5.0,
+// 				"bookmark": [{ id: 432534, time: 65876586 }]
+// 			}
+// 		]
+// 	},
+// 	{
+// 		name: "Dramas",
+// 		videos: [
+// 			{
+// 				"id": 65432445,
+// 				"title": "The Chamber",
+// 				"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+// 				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+// 				"rating": 4.0,
+// 				"bookmark": []
+// 			},
+// 			{
+// 				"id": 675465,
+// 				"title": "Fracture",
+// 				"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+// 				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+// 				"rating": 5.0,
+// 				"bookmark": [{ id: 432534, time: 65876586 }]
+// 			}
+// 		]
+// 	}
+// ],	
+
+
+console.log('playground works');
+
+
+
+
+var headings = [
+	quarter = {
+		clickViewType: "q", title: "October",
+		clickViewType: "q", title: "November",
+		clickViewType: "q", title: "December",
+	},
+	
+	monthly = {
+		clickViewType: "m", weekNumber: 42, daysInMonth: 31, numberOfWeeks: 6,
+		clickViewType: "m", weekNumber: 43, daysInMonth: 31, numberOfWeeks: 6,
+		clickViewType: "m", weekNumber: 44, daysInMonth: 31, numberOfWeeks: 6, 
+		clickViewType: "m", weekNumber: 45, daysInMonth: 31, numberOfWeeks: 6,
+		clickViewType: "m", weekNumber: 46, daysInMonth: 31, numberOfWeeks: 6,
+		clickViewType: "m", weekNumber: 47, daysInMonth: 31, numberOfWeeks: 6,
+	},
+	
+	dailys = {
+		dayOfWeek: undefined, clickViewType: "w",
+		dayOfWeek: "Monday", clickViewType: "w",
+		dayOfWeek: "Tuesday", clickViewType: "w",
+		dayOfWeek: "Wednesday", clickViewType: "w",
+		dayOfWeek: "Thursday", clickViewType: "w",
+		dayOfWeek: "Friday", clickViewType: "w",
+	}
+];
+
+
+console.log(headings);
+
+
