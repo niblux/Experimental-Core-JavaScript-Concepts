ed68457e2a546ad8a8724937e2b086d18d715b62
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2018-01-11 21:59:08 +0000

    Loops practice continued

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,81 +1,18 @@
-// var movieLists = [
-// 	{
-// 		name: "New Releases",
-// 		videos: [
-// 			{
-// 				"id": 70111470,
-// 				"title": "Die Hard",
-// 				"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
-// 				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-// 				"rating": 4.0,
-// 				"bookmark": []
-// 			},
-// 			{
-// 				"id": 654356453,
-// 				"title": "Bad Boys",
-// 				"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
-// 				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-// 				"rating": 5.0,
-// 				"bookmark": [{ id: 432534, time: 65876586 }]
-// 			}
-// 		]
-// 	},
-// 	{
-// 		name: "Dramas",
-// 		videos: [
-// 			{
-// 				"id": 65432445,
-// 				"title": "The Chamber",
-// 				"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
-// 				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-// 				"rating": 4.0,
-// 				"bookmark": []
-// 			},
-// 			{
-// 				"id": 675465,
-// 				"title": "Fracture",
-// 				"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
-// 				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-// 				"rating": 5.0,
-// 				"bookmark": [{ id: 432534, time: 65876586 }]
-// 			}
-// 		]
-// 	}
-// ],	
+// [4, 5, 1, 3] ➞ 5
 
+// [300, 200, 600, 150] ➞ 600
 
-console.log('playground works');
+var largeArr = [1000, 1001, 857, 1]; // 1001
 
+var largestNumber = Math.max(...largeArr)
 
+console.dir(largestNumber)
 
-
-var headings = [
-	quarter = {
-		clickViewType: "q", title: "October",
-		clickViewType: "q", title: "November",
-		clickViewType: "q", title: "December",
-	},
-	
-	monthly = {
-		clickViewType: "m", weekNumber: 42, daysInMonth: 31, numberOfWeeks: 6,
-		clickViewType: "m", weekNumber: 43, daysInMonth: 31, numberOfWeeks: 6,
-		clickViewType: "m", weekNumber: 44, daysInMonth: 31, numberOfWeeks: 6, 
-		clickViewType: "m", weekNumber: 45, daysInMonth: 31, numberOfWeeks: 6,
-		clickViewType: "m", weekNumber: 46, daysInMonth: 31, numberOfWeeks: 6,
-		clickViewType: "m", weekNumber: 47, daysInMonth: 31, numberOfWeeks: 6,
-	},
-	
-	dailys = {
-		dayOfWeek: undefined, clickViewType: "w",
-		dayOfWeek: "Monday", clickViewType: "w",
-		dayOfWeek: "Tuesday", clickViewType: "w",
-		dayOfWeek: "Wednesday", clickViewType: "w",
-		dayOfWeek: "Thursday", clickViewType: "w",
-		dayOfWeek: "Friday", clickViewType: "w",
-	}
-];
-
-
-console.log(headings);
-
-
+// function findLargestNum(arr) {
+  
+//   var counter = 0;
+  
+//   while (counter <= 0) {
+    
+//   }
+// }
