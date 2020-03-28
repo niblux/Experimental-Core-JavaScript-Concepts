fbfa69bdfe199252648a6e639eb8fea37b2dd5da
Nabil Elidrissi <nabil@paybase.io>
2020-03-28 00:02:42 +0000

    further work on reduce, sets of examples

--- app/js/arrayMethods/reduce.js
+++ app/js/arrayMethods/reduce.js
@@ -1,22 +1,24 @@
-const pokemon = [
-    { name: "charmander", type: "fire" },
-    { name: "squirtle", type: "water" },
-    { name: "bulbasaur", type: "grass" }
-];
+// A different type of reduce example
+const prices = [ 2.99, 5.99, 7.2, 15, 2000, 0.99 ] // should return [ 7.2, 15, 2000 ]
 
-const reduceItUp = (poke) => {
-    console.log(Object.prototype.toString.call(poke));
-    // poke.reduce((acc, { k, v }) => {
-    //     // console.log('val', val);
-    //     console.log('k', k.name);
-    //     console.log('v', v.type);
-    //     console.log('acc', acc);
-    // }, [])
+const higherPrices = prices.reduce((acc, val) => {
+    if(val > 6 ) {
+      acc.push(val);
+    } 
+    return acc;
+}, []);
 
-    // poke.reduce((acc, [k, v]) => {
-    //     acc.item.push({ name: k, item: v });
-    //     return acc;
-    // }, []);
+const array = [1, 3, 4, 7, 13];
+
+// Traditional for loop
+var total = 0;
+for (var i  = 0; i < array.length; i++){
+   total  += array[i];
 }
 
-console.log(reduceItUp(pokemon));
+// Composable reduce function
+const addNumbers = (acc, currVal) => acc + currVal;
+const reduce = array.reduce(addNumbers, 0)
+
+// Just a nice clean arrow function reduce w/implicit return
+const reduceOneLine = array.reduce((accumulator, currentValue) => accumulator + currentValue);
